53d121397698f1288266245541bee56a8b8a2bc6
Necroplod <necroplod@mail.ru>
2023-01-28 13:54:20 +0300

    updated welcome

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -1,3 +1,5 @@
+import datetime
+
 import settings
 
 import discord
@@ -11,26 +13,56 @@ class welcome(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        welcome = self.client.get_channel(settings.channels.welcome)
-        rules = f"<#959770387204415498>"
-        embed = discord.Embed(
-            title='',
-            description=f'''{member.mention}, Добро Пожаловать на сервер **Мистер Сферический**!       
-            Не забудь прочитать {rules}!''',
-            color=0x2ecc71
-        )
+        if member.guild.id == settings.misc.guild:
+            welcome = self.client.get_channel(settings.channels.welcome)
 
-        embed.set_image(url="https://i.ytimg.com/vi/nVzqh0fTARU/hqdefault.jpg")
-        embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        await welcome.send(embed=embed)
+            if member.bot:
+                integrations = await member.guild.integrations()
+                for integration in integrations:
+                    if isinstance(integration, discord.BotIntegration):
+                        if integration.application.user.name == member.name:
+                            inviter = integration.user
+                embed = discord.Embed(
+                    title='',
+                    description=f'''
+                    ●─────────────────●
+                    {member.mention}
+                    ●─────────────────● 
+                    Бот был добавлен {inviter.mention}''',
+                    color = 0x2ecc71,
+                    timestamp = datetime.datetime.now()
+                )
+            else:
+                embed = discord.Embed(
+                    title='',
+                    description=f'''
+                    ●─────────────────●
+                    {member.mention}
+                    ●─────────────────● 
+                    *Пламенный привет тебе друг!
+                    Заваривай кофеёк, и залетай в наилучший сервер на свете!*''',
+                    color = 0x2ecc71,
+                    timestamp = datetime.datetime.now()
+                )
+
+            embed.set_image(url="https://i.ytimg.com/vi/nVzqh0fTARU/hqdefault.jpg")
+            embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
+            await welcome.send(embed=embed)
+        else:
+            return
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         welcome = self.client.get_channel(settings.channels.welcome)
         embed = discord.Embed(
             title='',
-            description=f'**{member}** Покинул наш сервер :(',
-            color = 0x2ecc71
+            description=f'''
+            ●─────────────────●
+            {member.mention}
+            ●─────────────────●
+            **Покинул нас.**''',
+            color = 0x2ecc71,
+            timestamp = datetime.datetime.now()
         )
         embed.set_image(url="https://i.ytimg.com/vi/F_e2eknfMbU/hqdefault.jpg")
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
