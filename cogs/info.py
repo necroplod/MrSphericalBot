779968cf60c5804476ab089c84f9e1781c6c17cc
Necroplod <necroplod@mail.ru>
2023-02-03 23:16:32 +0300

    added new category in s.help

--- cogs/info.py
+++ cogs/info.py
@@ -76,8 +76,9 @@ class info(commands.Cog):
 
       embed.add_field(name = '<:pakistan:1046443315177984130>  Информация', value = f'`{prefix}help` `{prefix}alarm`', inline=False)
       embed.add_field(name = '<:moon:1051616411971231804>  Веселье', value = f'`{prefix}catkdk` `{prefix}kdk` `{prefix}kdkeat`', inline=False)
-      embed.add_field(name = '<:serbia:1051616011272589452>  ВПИ', value = f'`{prefix}msg`')
-      embed.add_field(name = '<:king:1005355877278154814>  Админские Штучки', value = f'`{prefix}giveaway` `{prefix}reroll` `{prefix}art` `{prefix}archive` `{prefix}panel` `{prefix}poll`', inline=False)
+      embed.add_field(name = '<:serbia:1051616011272589452>  ВПИ', value = f'`{prefix}msg`', inline=False)
+      embed.add_field(name = '<:kazahstan:1051609522642374727>  Розыгрыши и прочее', value = f'`{prefix}giveaway` `{prefix}reroll` `{prefix}poll`', inline=False)
+      embed.add_field(name = '<:king:1005355877278154814>  Админские Штучки', value = f'`{prefix}art` `{prefix}archive` `{prefix}panel`', inline=False)
       embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
       await ctx.send(embed=embed)
 
