fe4751bcd1b626794dfbdbce59c8b29169b78bce
Necroplod <necroplod@mail.ru>
2023-03-21 23:15:19 +0300

    rewritten commands to slashes

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,6 +1,7 @@
 import discord
 import random
 from discord.ext import commands
+from discord import app_commands
 
 from assets.images import Fun
 from assets.images import Stickers
@@ -11,134 +12,135 @@ class fun(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def catkdk(self, ctx):
+    @app_commands.command(name = "catkdk", description = "Мяу сука")
+    async def catkdk(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = random.choice(Fun.catkdk)
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def kdk(self, ctx):
+    @app_commands.command(name = "kdk", description = "Ням, ням пяченьки)")
+    async def kdk(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = random.choice(Fun.kdk)
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def kdkeat(self, ctx):
+    @app_commands.command(name = "kdkeat", description = "Куфаем пяченьки")
+    async def kdkeat(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Fun.kdkeat
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def кринж(self, ctx):
+    @app_commands.command(name = "кринж", description = "Шо ты высрал?")
+    async def кринж(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.кринж
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def лох(self, ctx):
+    @app_commands.command(name = "лох", description = "Лох.")
+    async def лох(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.лох
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def украина(self, ctx):
+    @app_commands.command(name = "украина", description = "Хрю-хрю, ой...")
+    async def украина(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.украина
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def да(self, ctx):
+    @app_commands.command(name = "да", description = "да.")
+    async def да(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.да
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def эй(self, ctx):
+    @app_commands.command(name = "эй", description = "Эй..")
+    async def эй(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.эй
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def бомба(self, ctx):
+    @app_commands.command(name = "бомба", description = "бууум")
+    async def бомба(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.бомба
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def осуждаю(self, ctx):
+    @app_commands.command(name = "осуждаю", description = "осуждаю, твич не бань")
+    async def осуждаю(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.осуждаю
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def быдло(self, ctx):
+    @app_commands.command(name = "быдло", description = "фу, быдло")
+    async def быдло(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.быдло
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def стоп(self, ctx):
+    @app_commands.command(name = "стоп", description = "Stop this shit")
+    async def стоп(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.стоп
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def господин(self, ctx):
+    @app_commands.command(name = "господин", description = "господин?")
+    async def господин(
+            self, interaction: discord.Interaction
+    ):
         embed = discord.Embed(color=discord.Color.random())
         url = Stickers.господин
         embed.set_image(url=url)
         embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-        embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
-        await ctx.message.delete()
-        await ctx.send(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
 async def setup(client):
     await client.add_cog(fun(client))
--- cogs/info.py
+++ cogs/info.py
@@ -2,62 +2,11 @@ import discord
 import typing
 import settings
 from discord.ext import commands
-from discord.ui.view import View
-from discord.ui.modal import Modal
+from discord import app_commands
+from typing import Union, Literal, Optional
 
-prefix = 's.'
+prefix = '/'
 
-class Alarm_arguments(Modal, title = '🦺 | Вызов модерации'):
-    reason = discord.ui.TextInput(
-        label = 'Причина:'
-    )
-    usr = discord.ui.TextInput(
-        label = 'Нарушитель:',
-        placeholder = 'ID или ник нарушителя, если такой есть. Если его нет, то оставьте это поле пустым!',
-        required = False
-    )
-    async def on_submit(self, interaction: discord.Interaction):
-        modchat = discord.utils.get(interaction.guild.channels, id=settings.channels.mod_chat)
-        this = discord.utils.get(interaction.guild.channels, name=interaction.channel.name)
-        if self.usr.value is None:
-            offender = 'Не указан'
-        if self.usr.value == '':
-            offender = 'Не указан'
-        if self.usr.value == ' ':
-            offender = 'Не указан'
-        else:
-            offender = self.usr.value
-
-
-        embed = discord.Embed(
-            title = '🦺 | Вызов модерации',
-            description = f'''
-            **• Пользователь:** <@{interaction.user.id}> | `{interaction.user.id}`
-            **• Канал:** <#{interaction.channel.id}>
-            
-            **• Нарушитель:** `{offender}`
-            **• Причина:** `{self.reason.value}`''',
-            color = 0xff6565
-        )
-        embed.set_footer(icon_url=settings.misc.avatar_url, text=settings.misc.footer)
-        answer = discord.Embed(
-            title = '🦺 | Вызов модерации',
-            description = f'<a:768563657390030971:1041076662546219168> <@{interaction.user.id}>, Вызов модерации успешно выполнен! Скоро они прибут на помощь и решат данную ситуацию.',
-            color=0xff6565
-        )
-        answer.set_footer(icon_url=settings.misc.avatar_url, text=settings.misc.footer)
-        await modchat.send('<@&1052168161304260629>', embed=embed)
-        await interaction.response.send_message(embed=answer)
-class Alarm(discord.ui.View):
-    def __init__(self, *, timeout=30, author):
-        self.author = author
-        super().__init__(timeout=timeout)
-
-    @discord.ui.button(emoji='📢', label = 'Вызов', style=discord.ButtonStyle.red)
-    async def start(self, interaction: discord.Interaction, button: discord.ui.Button):
-        if interaction.user != self.author:
-            await interaction.response.send_message(content="Эта кнопка была вызвана другим пользователем!", ephemeral=True)
-        await interaction.response.send_modal(Alarm_arguments())
 
 
 class info(commands.Cog):
@@ -65,31 +14,52 @@ class info(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
-    async def help(self, ctx):
+    @app_commands.command(name = "help", description = "Получите список команд")
+    async def help(
+            self, interaction: discord.Interaction
+    ):
       embed = discord.Embed(
           title = '📚 | Доступные Команды',
           description = '',
           colour = discord.Color.red()
       )
-      embed.set_author(name = ctx.author.name, icon_url = ctx.author.avatar.url)
 
       embed.add_field(name = '<:pakistan:1046443315177984130>  Информация', value = f'`{prefix}help` `{prefix}alarm`', inline=False)
       embed.add_field(name = '<:moon:1051616411971231804>  Веселье', value = f'`{prefix}catkdk` `{prefix}kdk` `{prefix}kdkeat`', inline=False)
       embed.add_field(name = '<:canada:1071734769395716116>  Стикеры', value = f'`{prefix}лох` `{prefix}украина` `{prefix}да` `{prefix}эй` `{prefix}бомба` `{prefix}осуждаю` `{prefix}быдло` `{prefix}стоп` `{prefix}кринж` `{prefix}господин`', inline = False)
-      embed.add_field(name = '<:kazahstan:1051609522642374727>  Розыгрыши и прочее', value = f'`{prefix}giveaway` `{prefix}reroll` `{prefix}poll`', inline=False)
-      embed.add_field(name = '<:king:1005355877278154814>  Админские Штучки', value = f'`{prefix}art` `{prefix}archive` `{prefix}panel` `{prefix}ticket`', inline=False)
+      embed.add_field(name = '<:kazahstan:1051609522642374727>  Розыгрыши и прочее', value = f'`{prefix}giveaway` `{prefix}poll`', inline=False)
+      embed.add_field(name = '<:king:1005355877278154814>  Админские Штучки', value = f'`{prefix}art` `{prefix}archive` `s.ticket` `{prefix}proof`', inline=False)
       embed.set_footer(icon_url = self.client.user.avatar.url, text = f'{self.client.user.name} | Все права защищены')
-      await ctx.send(embed=embed)
 
-    @commands.command()
-    async def alarm(self, ctx):
+      await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name = "alarm", description = "Оповестите модерацию о нарушении")
+    async def alarm(
+            self, interaction: discord.Interaction,
+            нарушитель: Union[discord.Member],
+            канал: Union[discord.TextChannel],
+            причина: str
+
+    ):
+        modchat = self.client.get_channel(settings.channels.mod_chat)
         embed = discord.Embed(
-            title = '🦺 | Вызов модерации',
-            description = '<a:768563657390030971:1041076662546219168> **Произошло нарушение правил? Вызовите модерацию!**',
-            color = 0xff6565
+            title='🦺 | Вызов модерации',
+            description=f'''
+            **• Пользователь:** <@{interaction.user.id}> | `{interaction.user.id}`
+            **• Канал:** <#{канал.id}>
+
+            **• Нарушитель:** <@{нарушитель.id}>
+            **• Причина:** `{причина}`''',
+            color=0xff6565
         )
         embed.set_footer(icon_url=settings.misc.avatar_url, text=settings.misc.footer)
-        await ctx.send(embed=embed, view = Alarm(author = ctx.author))
+        answer = discord.Embed(
+            title='🦺 | Вызов модерации',
+            description=f'<a:768563657390030971:1041076662546219168> <@{interaction.user.id}>, Вызов модерации успешно выполнен! Скоро они прибут на помощь и решат данную ситуацию.',
+            color=0xff6565
+        )
+        answer.set_footer(icon_url=settings.misc.avatar_url, text=settings.misc.footer)
+        await interaction.response.send_message(embed=answer)
+        await modchat.send('<@&1052168161304260629>', embed=embed)
 async def setup(client):
     await client.add_cog(info(client))
